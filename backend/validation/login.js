733fede7dffc338de53fdc2431adfe71c7d3c3cc
Shubham Verma <var.subham@gmail.com>
2020-07-27 20:52:34 +0530

    created login validation

---
+++ backend/validation/login.js
@@ -0,0 +1,26 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+function validateLoginInput(data){
+    let error = {};
+
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+    //Email Validation
+    if(Validator.isEmpty(data.email)){
+        error.email = "Email field required";
+    }
+    else if(!Validator.isEmail(data.email)){
+        error.email = "Invalid email";
+    }
+    //Password Validation
+    if(Validator.isEmpty(data.password)){
+        error.password = "Password field is require";
+    }
+
+    return {
+        error,
+        isValid: isEmpty(error),
+    }
+}
