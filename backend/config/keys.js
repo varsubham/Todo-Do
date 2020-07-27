c939de245dff1d00aaecfe1275055e9e0191e434
Shubham Verma <var.subham@gmail.com>
2020-07-28 01:06:26 +0530

    created backend for login auth

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://new-user_31:new-user_31@cluster0.4hi4b.gcp.mongodb.net/ToDoApp?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://new-user_31:new-user_31@cluster0.4hi4b.gcp.mongodb.net/ToDoApp?retryWrites=true&w=majority",
+    secretOrKey: "secret"
   };
