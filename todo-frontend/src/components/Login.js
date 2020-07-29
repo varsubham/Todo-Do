ab8bee97349c86135a0f0e81e0149392103a9c7c
Shubham Verma <var.subham@gmail.com>
2020-07-29 23:38:40 +0530

    created register and login

--- todo-frontend/src/components/Login.js
+++ todo-frontend/src/components/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Login(){
     return (
         <div>
-            from Login page
+            <form class = "contact" >
+        <div class="details" style = {{marginTop: "80px"}}>
+            <div class="red-bg">
+                <h1 style = {{color: "black", paddingTop: "30px", fontWeight: "bold", fontSize: "50px"}}>Sign in</h1>
+                <div class="form">
+                    <div class="inputbox">
+                        <input type="text" id="email" placeholder="Email Address"/>
+                    </div>
+                    <div class="inputbox">
+                        <input type="password" id="password" placeholder="Password"/>
+                    </div>
+                    <div class="inputbox1">
+                        <button class="button-main" style = {{backgroundImage: "linear-gradient(#ffffff, #bebebe)", width: "fit-content", padding: "10px 30px"}} ><span style= {{color: "black", fontSize: "xx-large", fontWeight: "bold"}}>Sign in</span></button>
+                    </div>
+                    <div style = {{borderBottom: "1px solid white", width: "90%", margin: "auto", paddingTop: "20px"}}></div>
+                </div>
+                
+                <div style ={{padding: "10px"}}>
+                <p style = {{color: "white", fontSize: "18px"}}>Don't have an accout? <Link to = '/register' style = {{color: "white"}}>Sign up</Link></p>
+                </div>
+            </div>
+        </div>
+    </form>
         </div>
     )
 }
