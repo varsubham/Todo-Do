8bfb454a4135691ae3604fee0a950b25191c3489
Shubham Verma <var.subham@gmail.com>
2020-08-16 16:00:34 +0530

    moved the delete icon on topleft

--- todo-frontend/src/components/main_comp/TasksComp.js
+++ todo-frontend/src/components/main_comp/TasksComp.js
@@ -17,12 +17,15 @@ class TasksComp extends React.Component{
         return (
             <div id = {this.props.task._id} className = "task-border" onMouseUp = {() => this.props.changePosition(this.props.task._id)} style = {{left: `${this.props.position.offSetLeft}px`, top: `${this.props.position.offSetTop}px`, width: '300px'}}>
                 <div className = "main-task">
-                    <div>
-                    <i id = {this.props.task._id+'header'} class="fa fa-arrows" style = {{fontSize: '36px', cursor: 'move'}}></i>
+                    <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 8px'}}>
+                        <div>
+                            <i id = {this.props.task._id+'header'} class="fa fa-arrows" style = {{fontSize: '36px', cursor: 'move'}}></i>
+                        </div>
+                        <div><i onClick = {() => this.props.ondelete(this.props.task._id)} class="fa fa-trash-o" style={{fontSize: '36px', cursor: 'pointer', visibility: this.props.progress_percent === 100 ? 'visible' : 'hidden'}}></i></div>
                     </div>
                     <div style = {{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                         <h4>{this.props.task.maintitle}</h4>
-                        <div><i onClick = {() => this.props.ondelete(this.props.task._id)} class="fa fa-trash-o" style={{fontSize: '36px', cursor: 'pointer', visibility: this.props.progress_percent === 100 ? 'visible' : 'hidden'}}></i></div>
+                        
                     </div>
                     <div className="progress" style = {{width: "100%", margin: "auto", height: "25px", backgroundColor: "white"}}>
                         <div className="progress-bar progress-bar-striped progress-bar-animated" style={{width: progress_percent, height: "25px"}}>
