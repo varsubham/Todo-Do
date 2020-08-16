54429f5b564ff5b560505fac299897201f735358
Shubham Verma <var.subham@gmail.com>
2020-08-16 20:51:31 +0530

    modified checkbox, label style

--- todo-frontend/src/components/main_comp/SubTaskComp.js
+++ todo-frontend/src/components/main_comp/SubTaskComp.js
@@ -5,7 +5,10 @@ class SubTaskComp extends React.Component{
         //console.log(this.props.subtask);
         return (
             <div className="checkbox" style = {{border: "none" , boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", backgroundColor: 'white', borderRadius: "4px", padding: "8px", margin: "8px 0px"}}>
-                <label><input id = {this.props.subtask.id} checked = {this.props.subtask.isCompleted} onChange = {() => this.props.checkboxClicked(this.props.subtask.id, this.props.task_id)} type="checkbox" value=""/> {this.props.subtask.text}</label>
+                <label style = {{display: 'flex',}}>
+                    <input style = {{verticalAlign: 'middle'}} id = {this.props.subtask.id} style ={{marginTop: '7px', marginRight: '6px'}} checked = {this.props.subtask.isCompleted} onChange = {() => this.props.checkboxClicked(this.props.subtask.id, this.props.task_id)} type="checkbox" value=""/> 
+                    <p style = {{verticalAlign:'middle'}}>{this.props.subtask.text}</p>
+                </label>
             </div>
         )
     }
