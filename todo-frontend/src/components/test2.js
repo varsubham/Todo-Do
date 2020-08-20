6f515a142d9c1119b8cecff7513a91725683ad97
Shubham Verma <var.subham@gmail.com>
2020-08-20 14:24:45 +0530

    updated register

--- todo-frontend/src/components/test2.js
+++ todo-frontend/src/components/test2.js
@@ -58,3 +58,35 @@
                         </div>
                     </div>
                 </form>
+
+
+<form className = "contact" onSubmit = {this.onSumbit}>
+                    <div className="details">
+                        <div className="red-bg">
+                            <h1 style = {{color: "black", paddingTop: "30px", fontWeight: "bold", fontSize: "50px"}}>Sign Up</h1>
+                            <div className="form">
+                                <div className="inputbox">
+                                    <input className={classnames("", {invalid: errors.name})} error = {errors.name} type="text" name = "name" value = {this.state.name} onChange = {this.onChangeListner} id="name" placeholder="Name"/>
+                                </div>
+                                <div className="inputbox">
+                                    <input className={classnames("", {invalid: errors.email})} error = {errors.email} type="text" name = "username" value = {this.state.username} onChange = {this.onChangeListner} id="email" placeholder="Email Address"/>
+                                </div>
+                                <div className="inputbox">
+                                    <input className={classnames("", {invalid: errors.password})} error = {errors.password} type="password" name = "password" value = {this.state.password} onChange = {this.onChangeListner} id="password1" placeholder="Password"/>
+                                </div>
+                                <div className="inputbox">
+                                    <input className={classnames("", {invalid: errors.password2})} error = {errors.password2} type="password" name = "password2" value = {this.state.password2} onChange = {this.onChangeListner} id="password2" placeholder="Confirm Password"/>
+                                </div>
+                                
+                                <div className="inputbox1">
+                                    <button className="button-main" type = "submit" style = {{backgroundImage: "linear-gradient(#ffffff, #bebebe)", width: "fit-content", padding: "10px 30px"}} ><span style= {{color: "black", fontSize: "xx-large", fontWeight: "bold"}}>Sign Up</span></button>
+                                </div>
+                                <div style = {{borderBottom: "1px solid white", width: "90%", margin: "auto", paddingTop: "20px"}}></div>
+                            </div>
+                            
+                            <div style ={{padding: "10px"}}>
+                            <p style = {{color: "white", fontSize: "18px"}}>Already have an accout? <Link to = '/login' style = {{color: "white"}}>Sign in</Link></p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
