6fa05cf7faecb3b8f8440ea29554415a80152f2d
Shubham Verma <var.subham@gmail.com>
2020-08-23 15:41:52 +0530

    formatted

--- todo-frontend/src/components/Landing.js
+++ todo-frontend/src/components/Landing.js
@@ -13,7 +13,7 @@ class Landing extends React.Component{
         return (
             <div>
                 <NavBar/>
-                <img className="wave2" src={wave}/>
+                <img className="wave2" src={wave} alt=""/>
                     <div className = "container">
                         <div className = "container-flex">
                             <div className = "landing-description">
@@ -23,7 +23,7 @@ class Landing extends React.Component{
                                     <li>Drag your tasks around the dashboard</li>
                                     <li>Check it when task is completed</li>
                                     <li style = {{marginBottom: '16px'}}>Delete it</li>
-                                    <li style = {{fontWeight: "bold"}}>Try Yourself <img width = "24px" src ={arrow}/></li>
+                                    <li style = {{fontWeight: "bold"}}>Try Yourself <img width = "24px" src ={arrow} alt=""/></li>
                                 </ul>
                                 <div className = "landing_btn">
                                     <Link to = '/register'>Get Started</Link>
