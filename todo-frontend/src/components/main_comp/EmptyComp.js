9d2b043a9e7a32249a780516069c51ab064cdd59
Shubham Verma <var.subham@gmail.com>
2020-08-24 00:32:05 +0530

    updated EmptyComp design

--- todo-frontend/src/components/main_comp/EmptyComp.js
+++ todo-frontend/src/components/main_comp/EmptyComp.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './emptyComp.css';
 const logo = require('../../images/logo3.svg');
-
+const empty = require('../img/empty.svg');
 class EmptyComp extends React.Component{
     render(){
         return (
             <div className = "empty-comp1">
-                <img src = {logo} alt = "" width = '300' />
+                <img src = {empty} alt = "" width = '300' />
                 <div style = {{textAlign: "center"}}>
-                    <h5>You have no pending Tasks</h5>
-                    <h6>Click below to add tasks</h6>
-                    <button>Add Tasks</button>
+                    <h2 style = {{marginBottom: '16px'}}>You Don't have any pending Task</h2>
+                    <h4>Click <i className="fa fa-plus"></i> on top to add tasks</h4>
+                    <hr/>
                 </div>
             </div>
         )
