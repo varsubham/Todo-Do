d833216f980c60dac81f60297a118387e7b2fab7
Shubham Verma <var.subham@gmail.com>
2020-08-23 15:22:37 +0530

    removed duplicate props warning

--- todo-frontend/src/components/main_comp/SubTaskComp.js
+++ todo-frontend/src/components/main_comp/SubTaskComp.js
@@ -5,7 +5,7 @@ class SubTaskComp extends React.Component{
         return (
             <div className="checkbox" style = {{border: "none" , boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", backgroundColor: 'white', borderRadius: "4px", padding: "8px", margin: "8px 0px"}}>
                 <label style = {{display: 'flex',}}>
-                    <input style = {{verticalAlign: 'middle'}} id = {this.props.subtask.id} style ={{marginTop: '7px', marginRight: '6px'}} checked = {this.props.subtask.isCompleted} onChange = {() => this.props.checkboxClicked(this.props.subtask.id, this.props.task_id)} type="checkbox" value=""/> 
+                    <input id = {this.props.subtask.id} style ={{marginTop: '7px', marginRight: '6px', verticalAlign: 'middle'}} checked = {this.props.subtask.isCompleted} onChange = {() => this.props.checkboxClicked(this.props.subtask.id, this.props.task_id)} type="checkbox" value=""/> 
                     <p style = {{verticalAlign:'middle'}}>{this.props.subtask.text}</p>
                 </label>
             </div>
