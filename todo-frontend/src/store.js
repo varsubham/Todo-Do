ece2b17d5e5f223a23cc6ab340d1b5f36287f68e
Shubham Verma <var.subham@gmail.com>
2020-08-24 20:02:31 +0530

    commented Redux Devtool for production

--- todo-frontend/src/store.js
+++ todo-frontend/src/store.js
@@ -8,7 +8,8 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    
+    // Commented below line for production, (Redux chrome devtool)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
